1823fd3bf2b1bae81591c6131e52b06c28f1bcf8
Rene Sauve <rdasauve@gmail.com>
2021-04-30 15:48:06 -0700

    gatsby build

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,10 +1,13 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 import { RestLink } from "apollo-link-rest"
+const fetch = require("node-fetch")
+global.Headers = fetch.Headers
 
 const uri = process.env.OMDB_API
 
 const restLink = new RestLink({
   uri: uri,
+  credentials: "same-origin",
   // Extra code to be added:
   typePatcher: {
     Movies: data => {
@@ -19,7 +22,6 @@ const restLink = new RestLink({
     },
   },
 })
-
 const client = new ApolloClient({
   cache: new InMemoryCache({}),
   link: restLink,
